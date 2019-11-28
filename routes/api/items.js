a796ad27cd7a619f37942aea3c02c5d766b6b75c
sanscomp <sanscomp@anslems-iMac.lan>
2019-11-28 18:45:14 +0000

    Add delete route to remove db items

--- routes/api/items.js
+++ routes/api/items.js
@@ -5,25 +5,34 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 // Create a database item
-router.post('/',(req, res)=>{
+router.post('/', (req, res) => {
     const newItem = new Item({
-        title:req.body.title,
-        jobTitle:req.body.jobTitle,
-        school:req.body.school,
-        salary:req.body.salary,
-        description:req.body.description
+        title: req.body.title,
+        jobTitle: req.body.jobTitle,
+        school: req.body.school,
+        salary: req.body.salary,
+        description: req.body.description
     });
     newItem.save().then(item => res.json(item))
 });
 
 // Get all database items
-router.get('/',(req, res)=>{
-    Item.find().then(items => res.json(items)) 
+router.get('/', (req, res) => {
+    Item.find().then(items => res.json(items))
 });
 
-// Get single database item
-router.get('/:id',(req, res)=>{
-    Item.findById(req.params.id).then(item => res.json(item)) 
+// Get a single database item
+router.get('/:id', (req, res) => {
+    Item.findById(req.params.id).then(item => res.json(item))
+        .catch(error => { res.status(404).json('Error, item not found, please check the id is correct. ' + error) })
 });
+
+// Delete database item
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove()
+            .then(res.json('Item has been deleted')))
+        .catch(error => { res.status(404).json('Item cannot be found' + error) })
+})
 
 module.exports = router;
