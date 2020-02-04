00b93587760ee00916e8baf0b2b2bbb7b58cff43
sanscomp <sanscomp@anslems-iMac.lan>
2020-02-04 18:57:20 +0000

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form';
 import Data from './components/Data';
+import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 
 function App() {
   return (
     <Router>
-      <h1>Job vacancies</h1>
+      <Navbar/>
       <Route path="/" exact component ={Data} />
       <Route path="/create" component ={Form} />
     </Router>
