3ae707076817e5b5bd3122bcdb87ac286101561f
sanscomp <sanscomp@anslems-iMac.lan>
2020-02-10 16:00:52 +0000

    Setup enzyme

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
