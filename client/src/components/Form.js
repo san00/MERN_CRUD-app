bb60bc3f5670422ca90757c5130f797c65ce09f5
sanscomp <sanscomp@anslems-iMac.lan>
2020-02-11 21:13:44 +0000

    Destructure props

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,80 +2,52 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 function Form() {
-    const [title, setTitle] = useState('')
-    const [jobTitle, setJobTitle] = useState('')
-    const [school, setSchool] = useState('')
-    const [salary, setSalary] = useState('')
-    const [description, setDescription] = useState('')
-
-    const onChangeTitle = (e) => {
-        setTitle(e.target.value)
-    }
-
-    const onChangeJobTitle = (e) => {
-        setJobTitle(e.target.value)
-    }
-
-    const onChangeSchool = (e) => {
-        setSchool(e.target.value)
-    }
-
-    const onChangeSalary = (e) => {
-        setSalary(e.target.value)
-    }
-
-    const onChangeDescription = (e) => {
-        setDescription(e.target.value)
+    const [values, setValues] = useState({ jobTitle: '', salary: '', description: '' })
+    const {jobTitle, salary, description} = values
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setValues({
+            ...values,
+            [name]: value
+        })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const listing = {
-            title: title,
             jobTitle: jobTitle,
-            school: school,
             salary: salary,
             description: description
         }
         axios.post('/api/items', listing)
-            .then(res => {console.log(res)})
+            .then(res => { console.log(res) })
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <h2>Create new listing</h2>
-            <div>
-                <label htmlFor='title'>Title:
-                <input type='text' id='title' placeholder='title' onChange={onChangeTitle} value={title}></input>
-                </label>
-            </div>
-
+          
             <div>
                 <label htmlFor='job title'>Job title:
-                <input type='text' id='job title' placeholder='job title' onChange={onChangeJobTitle} value={jobTitle}></input>
+                <input name='jobTitle' type='jobTitle' id='job title' placeholder='job title' onChange={handleChange} value={jobTitle}></input>
                 </label>
-            </div>
-
-            <div>
-                <label htmlFor='school'>School:
-                <input type='text' id='school' placeholder='school' onChange={onChangeSchool} value={school}></input>
-                </label>
-            </div>
+            </div> 
 
             <div>
                 <label htmlFor='salary'>Salary:
-                <input type='text' id='salary' placeholder='salary' onChange={onChangeSalary} value={salary}></input>
+                <input name='salary' type='salary' id='salary' placeholder='salary' onChange={handleChange} value={salary}></input>
                 </label>
             </div>
 
             <div>
                 <label htmlFor='description'>Description:
-                <input type='text' id='description' placeholder='description' onChange={onChangeDescription} value={description}></input>
+                <input name='description' type='description' id='description' placeholder='description' onChange={handleChange} value={description}></input>
                 </label>
-            </div>
-                <label htmlFor='submit'>
-                    <input type='submit' id='submit' value='save'></input>
-                </label>
+            </div> 
+            <label htmlFor='submit'>
+                <input type='submit' id='submit' value='save'></input>
+            </label>
         </form>
     )
 }
