c165aad2c3b0b7a8771eb5ecf18795e153490944
sanscomp <sanscomp@anslems-iMac.lan>
2020-03-02 11:59:45 +0000

    Use custom hook in form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,20 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import axios from 'axios'
+import useForm from './useForm';
 
 function Form() {
-    const [values, setValues] = useState({ jobTitle: '', salary: '', description: '' })
-    const {jobTitle, salary, description} = values
-    
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const submit = () => {
         const listing = {
             jobTitle: jobTitle,
             salary: salary,
@@ -24,29 +13,48 @@ function Form() {
             .then(res => { console.log(res) })
     }
 
+    const { handleChange, handleSubmit, values } = useForm(submit)
+    const { jobTitle, salary, description } = values
+
     return (
         <form onSubmit={handleSubmit}>
-            <h2>Create new listing</h2>
-          
+            <h2>Create new vacancy</h2>
             <div>
                 <label htmlFor='job title'>Job title:
-                <input name='jobTitle' type='jobTitle' id='job title' placeholder='job title' onChange={handleChange} value={jobTitle}></input>
+                <input name='jobTitle'
+                        type='jobTitle'
+                        id='job title'
+                        placeholder='job title'
+                        onChange={handleChange}
+                        value={jobTitle}></input>
                 </label>
-            </div> 
+            </div>
 
             <div>
                 <label htmlFor='salary'>Salary:
-                <input name='salary' type='salary' id='salary' placeholder='salary' onChange={handleChange} value={salary}></input>
+                <input name='salary'
+                        type='salary'
+                        id='salary'
+                        placeholder='salary'
+                        onChange={handleChange}
+                        value={salary}></input>
                 </label>
             </div>
 
             <div>
                 <label htmlFor='description'>Description:
-                <input name='description' type='description' id='description' placeholder='description' onChange={handleChange} value={description}></input>
+                <input name='description'
+                        type='description'
+                        id='description'
+                        placeholder='description'
+                        onChange={handleChange}
+                        value={description}></input>
                 </label>
-            </div> 
+            </div>
             <label htmlFor='submit'>
-                <input type='submit' id='submit' value='save'></input>
+                <input type='submit'
+                    id='submit'
+                    value='save'></input>
             </label>
         </form>
     )
