33a6bc08a716960999b264f77f6b01682c543690
sanscomp <sanscomp@anslems-iMac.lan>
2020-03-22 19:11:48 +0000

    Add Javascript form validation

--- client/src/components/useForm.js
+++ client/src/components/useForm.js
@@ -4,27 +4,52 @@ const useForm = (callback) => {
     const [values, setValues] = useState({
         jobTitle: '',
         salary: '',
-        description: ''
+        description: '',
+        validationErrors: {}
     })
 
     const handleChange = (e) => {
         const { name, value } = e.target
         setValues({
             ...values,
-            [name]: value
+            [name]: value,
         })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         callback()
+        validate()
+    }
+
+    const validate = () => {
+        const { jobTitle, salary, description } = values
+        const errors = {}
+
+        if (!jobTitle) {
+            errors.jobTitle = 'Job title cannot be empty!'
+        } else if (!/\D/gi.test(jobTitle)) {
+            errors.jobTitle = 'Job title must contain letters!'
+        }
+        if (!salary) {
+            errors.salary = 'Salary cannot be empty!'
+        } else if (!/\d/gi.test(salary)) {
+            errors.salary = 'Salary must be a number!'
+        }
+        if (!description) {
+            errors.description = 'Description cannot be empty!'
+        }
+        else if (description.length < 3) {
+            errors.description = 'Please provide at least 3 characters!'
+        }
+
+        setValues({ validationErrors: errors })
     }
 
     return {
         handleChange,
         handleSubmit,
-        values, setValues
+        values
     }
-
 }
 export default useForm
