ce1748e66faa2f72917823170cb9b5a493f22645
sanscomp <sanscomp@anslems-iMac.lan>
2020-06-15 15:05:33 +0100

    Rename Axios test function

--- client/src/actions/hookActions.js
+++ client/src/actions/hookActions.js
@@ -1,26 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const mockAxiosCall = async (setVacancy) => {
-    const response = await axios.get('/api/items')
-    setVacancy(response.data)
-}
-
-export const mockDeleteAxiosCall = async (setRemove) => {
-    const response = await axios.delete('/api/items/:id')
-    setRemove(response.data)
-}
-
-export const mockEditAxiosCall = async (setValues) => {
-    const response = await axios.put('/api/items/:id')
-    setValues(response.data)
-}
-
-export const mockCreateAxiosCall = async (setValues) => {
-    const response = await axios.post('/api/items')
-    setValues(response.data)
-}
+export const getDatabaseItems = async (setVacancy) => {
+  const response = await axios.get("/api/items");
+  setVacancy(response.data);
+};
 
 // default export for mocking convenience
 export default {
-    mockAxiosCall, mockDeleteAxiosCall, mockEditAxiosCall
-}
+  getDatabaseItems,
+};
