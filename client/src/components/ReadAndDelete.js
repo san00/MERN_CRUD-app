e98b68a7aabd11269db41029f9e0722ac3a9dc1e
sanscomp <sanscomp@anslems-iMac.lan>
2020-06-15 15:09:48 +0100

    Test ReadAndDelete component renders

--- client/src/components/ReadAndDelete.js
+++ client/src/components/ReadAndDelete.js
@@ -1,51 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import DisplayJob from './DisplayJob'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DisplayJob from "./DisplayJob";
 
 function ReadAndDelete() {
-    const [vacancy, setVacancy] = useState([])
-    const [remove, setRemove] = useState([])
+  const [vacancy, setVacancy] = useState([]);
+  const [remove, setRemove] = useState([]);
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            axios.get('/api/items')
-                .then(res => {
-                    setVacancy(res.data)
-                }).catch((error) => {
-                    console.error(error);
-                })
-        }
-        fetchItems()
+  useEffect(() => {
+    const fetchItems = async () => {
+      axios
+        .get("/api/items")
+        .then((res) => {
+          setVacancy(res.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    fetchItems();
+  }, [remove, setVacancy]);
 
-    }, [remove, setVacancy])
+  const removeItem = (e, id) => {
+    e.preventDefault();
+    axios
+      .delete(`/api/items/${id}`)
+      .then((res) => console.log(res.data))
+      .catch((error) => {
+        console.error(error);
+      });
+    setRemove(vacancy.filter((item) => item.id !== id));
+  };
 
-
-    const removeItem = (e, id) => {
-        e.preventDefault();
-        axios.delete(`/api/items/${id}`)
-            .then(res => console.log(res.data)
-            )
-            .catch((error) => {
-                console.error(error);
-            })
-        setRemove(vacancy.filter(item => item.id !== id))
-    }
-
-    const jobInfo = vacancy.length === 0 ? (<h2 className='displayJob displayJob__message'> There are currently no jobs listed</h2>) : vacancy.map((jobPost, id, index) => {
+  const jobInfo =
+    vacancy.length === 0 ? (
+      <h2 className="displayJob displayJob__message">
+        {" "}
+        There are currently no jobs listed
+      </h2>
+    ) : (
+      vacancy.map((jobPost, id, index) => {
         return (
-            <DisplayJob key={id}
-                jobPost={jobPost}
-                index={id}
-                removeItem={removeItem} />
-        )
-    })
+          <DisplayJob
+            key={id}
+            jobPost={jobPost}
+            index={id}
+            removeItem={removeItem}
+          />
+        );
+      })
+    );
 
-    return (
-        <section className='content__container displayJob__container'>
-            <p className='displayJob__header'>Vacancies</p>
-            {jobInfo}
-        </section>
-    )
+  return (
+    <section
+      data-test="component-ReadAndDelete"
+      className="content__container displayJob__container"
+    >
+      <p className="displayJob__header">Vacancies</p>
+      {jobInfo}
+    </section>
+  );
 }
 
-export default ReadAndDelete
+export default ReadAndDelete;
